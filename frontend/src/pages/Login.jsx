9483b476cfea318c3c1b7dc1fe9817cd622e8f85
dheerajs7 <dheeraj.salvi293@gmail.com>
2024-09-18 17:24:45 +0530

    login logout setup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import useLogin from '../hooks/useLogin';
 
 const Login = () => {
+
+     const [email, setEmail] =useState("")
+     const [password,setPassword] =useState("")
+         
+     const {login,loading}= useLogin()
+    const handleSubmit =async(e)=>{
+        e.preventDefault()
+
+        await login(email,password)
+    }
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
     <div className='w-full p-6 rounded-lg shadow-md bg-gray-200'>
@@ -9,19 +21,28 @@ const Login = () => {
     <span className='text-blue-700'> ChatterUp</span>
     </h1>
      
-     <form>
+     <form onSubmit={handleSubmit}>
         <div>
             <label className='label p-2'>
                 <span className='text-base label-text'>Email</span>
             </label>
-            <input type='email' placeholder='Enter Your Email' className='input input-bordered w-full  h-10 max-w-xs' />
+            <input type='email' placeholder='Enter Your Email'
+              value ={email}
+              onChange={(e)=>setEmail(e.target.value)}
+             className='input input-bordered w-full  h-10 max-w-xs' />
         </div>
 
         <div>
             <label className='label p-2'>
                 <span className='text-base label-text'>Password</span>
             </label>
-            <input type='password' placeholder='Enter Your Password' className='input input-bordered w-full  h-10 max-w-xs' />
+            <input type='password' 
+            placeholder='Enter Your Password'
+            className='input input-bordered w-full  h-10 max-w-xs' 
+            value ={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            />
+             
         </div>
 
         <Link to={"/signup"} className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
@@ -29,7 +50,15 @@ const Login = () => {
         </Link>
 
         <div>
-            <button className='btn btn-block btn-sm mt-2 bg-blue-700 font-b text-white'>Login</button>
+            <button className='btn btn-block btn-sm mt-2 bg-blue-700 font-b
+             text-white' disabled={loading}>
+              {loading?(
+                <span className='"loading loading-spinner'></span>
+              ):(
+                  "Login"
+              )
+              }
+             </button>
         </div>
      </form>
     
